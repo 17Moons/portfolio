5a770b65a1963cb68b6f587e1f86bc5c50d4d7f4
17Moons <vaibhav.singh.cd.eee20@itbhu.ac.in>
2022-02-07 21:58:33 +0530

    deleted unecessary files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import AnimatedText from "./AnimatedText";
-import "./styles.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Landing from "./Landing.jsx";
 
-export default function App() {
-  const [replay, setReplay] = useState(true);
-  // Placeholder text data, as if from API
-  const placeholderText = [
-    { type: "heading1", text: "Hello" }
-  ];
 
-  const container = {
-    visible: {
-      transition: {
-        staggerChildren: 0.025
-      }
-    }
-  };
 
-  // Quick and dirt for the example
-  const handleReplay = () => {
-    setReplay(!replay);
-    setTimeout(() => {
-      setReplay(true);
-    }, 600);
-  };
-
+function App(){
   return (
-    <motion.div
-      className="App"
-      initial="hidden"
-      // animate="visible"
-      animate={replay ? "visible" : "hidden"}
-      variants={container}
-    >
-      <div className="container">
-        {placeholderText.map((item, index) => {
-          return <AnimatedText {...item} key={index} />;
-        })}
-      </div>
-      <button onClick={handleReplay}>
-        Replay <span>⟲</span>
-      </button>
-    </motion.div>
+    <div>
+    <Router>
+      <Switch>
+       <Route exact path="/">
+         <Landing />
+     </Route>
+      </Switch>
+    </Router>
+  </div>
   );
+
 }
+
+export default App;
